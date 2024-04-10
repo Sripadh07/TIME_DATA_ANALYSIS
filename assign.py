345c2c4e36764643a9938c58c7f3b9f2384ab0cf
Sripadh07 <128777341+Sripadh07@users.noreply.github.com>
2024-04-10 16:01:44 +0530

    Update assign.py

--- assign.py
+++ assign.py
@@ -6,12 +6,17 @@ data = pd.read_csv('SBIN_Data.csv')
 data['TIME'] = pd.to_datetime(data['Time'])
 
 def rank_volume(data):
-    # TAKING DATA ACCORDING TO THE DATES
-    Duration = data.sort_values(by='Date', ascending=False).head(5)
-    # RANKING THE VOLUME COLUMN
-    data['Rank'] = Duration['Volume'].rank(ascending=False)
-    return data
-   
+    # GETTING THE DATE OF THE CURRENT GROUP
+    Initial_date= data['Date'].iloc[0]
+    # GETTING THE DATE RANGE FOR THE LAST 5 DAYS
+    Duration= pd.date_range(end=Initial_date, periods=5, freq='D')
+    # FILTERING THE DATA FOR THE LAST 5 DAYS AND THE SAME TIME AS THE CURRENT DATA
+    Duration_data= data[(data['Date'].isin(Duration)) & (data['TIME'].dt.time == data['TIME'].iloc[0].time())] 
+    # RANKING VOLUME WITHIN THE LAST 5 DAYS DATA
+    data['rank']= data['Volume'].rank(ascending=False)
+    return data 
+
+
 # GROUPING THE DATA ACCORDING TO THE TIME
 ranked_data= data.groupby(data['TIME'].dt.time).apply(rank_volume).reset_index(drop=True)
 print(ranked_data)
